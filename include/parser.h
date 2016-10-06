6bdf6ab05daecce7ede65fce947593515986a944
Ray Brooks <rbrooks@microsoft.com>
2016-10-06 01:37:37 +0100

    cleaned up parser

--- include/parser.h
+++ include/parser.h
@@ -10,30 +10,29 @@ typedef struct NameList {
 
 #endif
 
-void cc_readtok(void);
+void parser_readtok(void);
 
-void cc_set_action_reg(void);
-void cc_concat_action_reg(char* split);
-void cc_set_word_reg(void);
-void cc_empty_word_reg(void);
+void parser_set_action_reg(void);
+void parser_concat_action_reg(char* split);
+void parser_set_word_reg(void);
+void parser_empty_word_reg(void);
 
-void cc_empty_name_list(void);
-void cc_push_to_cc_word_reg(void);
+void parser_empty_name_list(void);
+void parser_add_word_reg_to_name_list(void);
 
-int cc_peek(enum TokenType type);
-int cc_peakVal(char *s);
-int cc_accept(enum TokenType type);
+int parser_peek(enum TokenType type);
+int parser_accept(enum TokenType type);
 
-void cc_error(enum ErrorType error, char* val);
+void parser_error(enum ErrorType error, char* val);
 
-void cc_item(void);
-void cc_action(InstructionList** instructions);
+void parser_item(void);
+void parser_action(InstructionList** instructions);
 
-void cc_command(InstructionList** instructions);
-void cc_commands(InstructionList** instructions);
+void parser_command(InstructionList** instructions);
+void parser_commands(InstructionList** instructions);
 
-void cc_quit(void);
-void cc_eol(void);
+void parser_quit(void);
+void parser_eol(void);
 
 void free_parser(InstructionList** instructions);
 enum RunState parse(Token** tokens, InstructionList** instructions);
--- src/parser.c
+++ src/parser.c
@@ -14,182 +14,182 @@
 #include "../include/parser.h"
 
 enum RunState ERR = SE_OK;
-int cc_counter = 0;
-char cc_word_reg[MAXNAMESZ];
-char cc_action_reg[MAXNAMESZ];
-NameList* cc_name_stack;
+int parser_counter = 0;
+char parser_word_reg[MAXNAMESZ];
+char parser_action_reg[MAXNAMESZ];
+NameList* parser_name_stack;
 Token* tokenList;
 InstructionList* lastInstruction;
 
 extern Item* items;
 extern Actions* actions;
 
-void cc_readtok() {
+void parser_readtok() {
   tokenList = tokenList->next;
-  cc_counter++;
+  parser_counter++;
 }
 
-void cc_set_action_reg() {
-  memset(cc_action_reg, 0, MAXNAMESZ);
-  strcpy(cc_action_reg, tokenList->val);
+void parser_set_action_reg() {
+  memset(parser_action_reg, 0, MAXNAMESZ);
+  strcpy(parser_action_reg, tokenList->val);
 }
 
-void cc_concat_action_reg(char* split) {
-  strcat(cc_action_reg, split);
-  strcat(cc_action_reg, tokenList->val);
+void parser_concat_action_reg(char* split) {
+  strcat(parser_action_reg, split);
+  strcat(parser_action_reg, tokenList->val);
 }
 
-void cc_set_word_reg() {
-  if (strlen(cc_word_reg)) {
-    strcat(cc_word_reg, " ");
+void parser_set_word_reg() {
+  if (strlen(parser_word_reg)) {
+    strcat(parser_word_reg, " ");
   }
-  strcat(cc_word_reg, tokenList->val);
+  strcat(parser_word_reg, tokenList->val);
 }
 
-void cc_empty_word_reg() {
-  memset(cc_word_reg, 0, MAXNAMESZ);
+void parser_empty_word_reg() {
+  memset(parser_word_reg, 0, MAXNAMESZ);
 }
 
-void cc_empty_name_list() {
-  SGLIB_LIST_MAP_ON_ELEMENTS(NameList, cc_name_stack, mappedName, next, {
-    SGLIB_LIST_DELETE(NameList, cc_name_stack, mappedName, next);
+void parser_empty_name_list() {
+  SGLIB_LIST_MAP_ON_ELEMENTS(NameList, parser_name_stack, mappedName, next, {
+    SGLIB_LIST_DELETE(NameList, parser_name_stack, mappedName, next);
     free(mappedName);
   });
 }
 
-void cc_push_word_reg_to_item_reg() {
+void parser_add_word_reg_to_name_list() {
   struct NameList* newName = malloc(sizeof(struct NameList));
-  strcpy(newName->name, cc_word_reg);
+  strcpy(newName->name, parser_word_reg);
 
-  SGLIB_LIST_ADD(struct NameList, cc_name_stack, newName, next);
+  SGLIB_LIST_ADD(struct NameList, parser_name_stack, newName, next);
 
-  cc_empty_word_reg();
+  parser_empty_word_reg();
 }
 
-int cc_peek(enum TokenType type) {
+int parser_peek(enum TokenType type) {
   if (tokenList->type == type) {
     return 1;
   }
   return 0;
 }
 
-int cc_peekVal(char *s) {
+int parser_peekVal(char *s) {
   return (strcmp(tokenList->val, s) == 0);
 }
 
-int cc_accept(enum TokenType type) {
-  if (cc_peek(type)) {
+int parser_accept(enum TokenType type) {
+  if (parser_peek(type)) {
     switch (type) {
       case TOK_WORD:
-        cc_set_word_reg();
+        parser_set_word_reg();
         break;
       case TOK_VERB:
-        cc_set_action_reg();
+        parser_set_action_reg();
         break;
       case TOK_PREPOSITION:
       case TOK_CONJUNCTION:
-        cc_concat_action_reg(" ");
+        parser_concat_action_reg(" ");
         break;
     }
-    cc_readtok();
+    parser_readtok();
     return 1;
   }
   return 0;
 }
 
-void cc_error(enum ErrorType error, char* val) {
+void parser_error(enum ErrorType error, char* val) {
   ERR = SE_WARNING;
 
   create_error(error, val);
 
-  cc_readtok();
+  parser_readtok();
 }
 
-void cc_item() {
-  cc_accept(TOK_PRONOUN);
-  if (cc_peek(TOK_WORD)) {
-    while (cc_peek(TOK_WORD)) {
-      cc_accept(TOK_WORD);
+void parser_item() {
+  parser_accept(TOK_PRONOUN);
+  if (parser_peek(TOK_WORD)) {
+    while (parser_peek(TOK_WORD)) {
+      parser_accept(TOK_WORD);
     }
-    cc_push_word_reg_to_item_reg();
+    parser_add_word_reg_to_name_list();
   } else {
-    cc_error(CC_ITEM_EXPECTED, tokenList->val);
+    parser_error(CC_ITEM_EXPECTED, tokenList->val);
   }
 }
 
-void cc_action(InstructionList** instructions) {
+void parser_action(InstructionList** instructions) {
   Token* token = tokenList;
   Actions* action = NULL;
   Location* location = NULL;
   Item* item = NULL;
   char numItems = 0;
 
-  cc_accept(TOK_VERB);
-  cc_accept(TOK_PREPOSITION);
+  parser_accept(TOK_VERB);
+  parser_accept(TOK_PREPOSITION);
 
-  if (cc_peek(TOK_WORD) || cc_peek(TOK_PRONOUN)) {
-    cc_item();
-    if (cc_accept(TOK_CONJUNCTION)) {
-      cc_item();
+  if (parser_peek(TOK_WORD) || parser_peek(TOK_PRONOUN)) {
+    parser_item();
+    if (parser_accept(TOK_CONJUNCTION)) {
+      parser_item();
     }
   }
 
-  SGLIB_LIST_REVERSE(NameList, cc_name_stack, next);
-  SGLIB_LIST_LEN(NameList, cc_name_stack, next, numItems);
+  SGLIB_LIST_REVERSE(NameList, parser_name_stack, next);
+  SGLIB_LIST_LEN(NameList, parser_name_stack, next, numItems);
 
   if (numItems > 0) {
-    //ref to first item in cc_name_stack will now be at head
-    item = findItemByName(cc_name_stack->name);
-    location = findLocationByName(cc_name_stack->name);
+    //ref to first item in parser_name_stack will now be at head
+    item = findItemByName(parser_name_stack->name);
+    location = findLocationByName(parser_name_stack->name);
 
     if (item != NULL || location != NULL) {
-      action = findActionByNameAndItem(actions, item, cc_action_reg);
+      action = findActionByNameAndItem(actions, item, parser_action_reg);
       if (action == NULL) {
-        action = findDefaultActionByName(actions, cc_action_reg);
+        action = findDefaultActionByName(actions, parser_action_reg);
       }
     } else {
-      cc_error(CC_ITEM_EXPECTED, cc_name_stack->name);
+      parser_error(CC_ITEM_EXPECTED, parser_name_stack->name);
     }
   } else {
-    action = findActionByName(actions, cc_action_reg);
+    action = findActionByName(actions, parser_action_reg);
   }
 
   if (action != NULL) {
-    lastInstruction = inst_insert(instructions, action->instructions, lastInstruction, numItems > 0 ? cc_name_stack->name : NULL, numItems > 1 ? cc_name_stack->next->name : NULL);
+    lastInstruction = inst_insert(instructions, action->instructions, lastInstruction, numItems > 0 ? parser_name_stack->name : NULL, numItems > 1 ? parser_name_stack->next->name : NULL);
   } else {
-    cc_error(CC_NO_SUCH_ACTION, token->val);
+    parser_error(CC_NO_SUCH_ACTION, token->val);
   }
 }
 
-void cc_command(InstructionList** instructions) {
-  cc_empty_name_list();
+void parser_command(InstructionList** instructions) {
+  parser_empty_name_list();
 
-  if (cc_peek(TOK_VERB)) {
-    cc_action(instructions);
-  } else if (cc_accept(TOK_QUIT)) {
-    cc_quit();
+  if (parser_peek(TOK_VERB)) {
+    parser_action(instructions);
+  } else if (parser_accept(TOK_QUIT)) {
+    parser_quit();
   } else {
-    cc_error(CC_ERR_COMMAND_EXPECTED, tokenList->val);
+    parser_error(CC_ERR_COMMAND_EXPECTED, tokenList->val);
   }
 }
 
-void cc_commands(InstructionList** instructions) {
-  cc_command(instructions);
+void parser_commands(InstructionList** instructions) {
+  parser_command(instructions);
 
-  if (cc_accept(TOK_EOL)) {
-    cc_eol();
-  } else if (cc_accept(TOK_COMPLEX)) {
-    cc_commands(instructions);
+  if (parser_accept(TOK_EOL)) {
+    parser_eol();
+  } else if (parser_accept(TOK_COMPLEX)) {
+    parser_commands(instructions);
   } else {
-    cc_error(CC_ERR_END_OF_COMMAND_EXPECTED, tokenList->val);
+    parser_error(CC_ERR_END_OF_COMMAND_EXPECTED, tokenList->val);
   }
 }
 
-void cc_quit() {
-  cc_error(CC_QUIT, tokenList->val);
+void parser_quit() {
+  parser_error(CC_QUIT, tokenList->val);
 }
 
-void cc_eol() {
+void parser_eol() {
   return;
 }
 
@@ -206,11 +206,11 @@ enum RunState parse(Token** tokens, InstructionList** instructions) {
   tokenList = *tokens;
 
   ERR = SE_OK;
-  cc_counter = 0;
   lastInstruction = *instructions;
+  parser_counter = 0;
 
   while(tokenList != NULL && ERR == SE_OK) {
-    cc_commands(instructions);
+    parser_commands(instructions);
   }
 
   //reset the token start pointer
